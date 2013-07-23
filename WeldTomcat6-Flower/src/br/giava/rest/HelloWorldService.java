189a5d7f28f2283a683ca2a3c8f6672343ee203a
hernand.azevedo@gmail.com <hernand.azevedo@gmail.com@5f6c7f81-0c3b-affb-5606-68de26620b52>
2013-07-23 18:11:59 +0000

    rest json

--- WeldTomcat6-Flower/src/br/giava/rest/HelloWorldService.java
+++ WeldTomcat6-Flower/src/br/giava/rest/HelloWorldService.java
@@ -11,6 +11,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import by.giava.model.RespostaSync;
+
 import by.giava.model.Track;
 
 @Path("/hello")
@@ -27,26 +29,30 @@ public class HelloWorldService {
 	}
 	
 	@GET
-	@Path("/get")
+	@Path("/get/{imei}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Track> getTrackInJSON() {
+	public Response getTrackInJSON(@PathParam("imei") String imei) {
  
 		List<Track> lista = new ArrayList<Track>();
-		
+		System.out.println("imei: "+imei);
 		Track track = new Track();
 		track.setTitle("Enter Sandman");
 		track.setSinger("Metallica");
 		
 		
 		Track track2 = new Track();
-		track.setTitle("Enter Sandman2");
-		track.setSinger("Metallica2");
+		track2.setTitle("Enter Sandman2");
+		track2.setSinger("Metallica2");
 		
 		lista.add(track);
 		lista.add(track2);
 		
- 
-		return lista;
+		RespostaSync resposta = new RespostaSync();
+		resposta.setCodigoStatus(0);
+		resposta.setMessageStatus("OK");
+		resposta.setLista(lista);
+		
+		return Response.status(200).entity(resposta).build();
  
 	}
  
